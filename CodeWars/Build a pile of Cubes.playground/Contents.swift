ac0baf05774dbf29fa2ca32db9fc5ccaad4c5408
ikosmich <ikosmich@yandex.ru>
2022-12-08 00:25:41 +0300

    done

---
+++ CodeWars/Build a pile of Cubes.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+func findNb(_ number: Int) -> Int {
+  var n = number
+    var count = 0
+    while n > 0 {
+        n -= Int(pow(Double(count), Double(3)))
+        count += 1
+    }
+    return n == 0 ? count - 1 : -1
+}
