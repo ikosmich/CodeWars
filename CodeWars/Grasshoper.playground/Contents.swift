d0285095a365c58d9ecd0e8cebc676aaba001ece
ikosmich <ikosmich@yandex.ru>
2022-12-07 22:43:30 +0300

    CodeWars

---
+++ CodeWars/Grasshoper.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+//Task
+//
+//Create these three variables and store the appropriate values using the table above:
+//laLigaGoals
+//championsLeagueGoals
+//copaDelReyGoals
+//Create a fourth variable named totalGoals that stores the sum of all of Messi's goals for this year.
+
+var laLigaGoals = 43
+var championLeagueGoals = 10
+var copaDelReyGoals = 5
+
+var totalGoals = laLigaGoals + championLeagueGoals + copaDelReyGoals
