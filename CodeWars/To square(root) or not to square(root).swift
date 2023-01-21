9e56e35d8c3213d3ca2d2c3eefa1d924f292629e
ikosmich <86788268+ikosmich@users.noreply.github.com>
2023-01-21 18:27:46 +0300

    Add files via upload

---
+++ CodeWars/To square(root) or not to square(root).swift
@@ -0,0 +1,18 @@
+// Write a method, that will get an integer array as parameter and will process every number from this array.
+
+// Return a new array with processing every number of the input-array like this:
+
+// If the number has an integer square root, take this, otherwise square the number.
+
+// Example
+// [4,3,9,7,2,1] -> [2,9,3,49,4,1]
+// Notes
+// The input array will always contain only positive numbers, and will never be empty or null.
+
+func squareOrSquareRoot(_ input: [Int]) -> [Int] {
+  var result: [Int] = []
+  input.forEach { num in 
+    Int(exactly: Double(num).squareRoot()) != nil ? result.append(Int(Double(num).squareRoot())) : result.append(num * num)
+  }
+  return result
+}
